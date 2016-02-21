7725f245e82794ee8de4e132e3593ffbe48317a9
Daniel Carles <danielcarles@gmail.com>
2016-02-21 20:06:46 +0000

    minor styling

--- FizzBuzz.MVC/Models/FizzBuzzViewModel.cs
+++ FizzBuzz.MVC/Models/FizzBuzzViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ namespace FizzBuzz.MVC.Models
     {
         [Range(1, 1000)]
         [Required]
+        [DisplayName("Value")]
         public int UserPosition { get; set; }
         public IPagedList<String> FizzBuzzNumbers { get; set; }
     }
