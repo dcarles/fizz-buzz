58270f95bee8aa3c0a07ebb75521f6ad5fe7fd3a
Daniel Carles <danielcarles@gmail.com>
2016-02-21 18:31:26 +0000

    Finished unit tests for step 1 and FizzBuzz class ready to move to MVC project

--- FizzBuzz.Tests/FizzBuzzTests.cs
+++ FizzBuzz.Tests/FizzBuzzTests.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Text;
 using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 
 namespace FizzBuzz.Tests
@@ -54,7 +55,6 @@ namespace FizzBuzz.Tests
         [Test]
         public void GetFizzBuzz_Should_Return_List_With_One_Two_Fizz_If_Input_Is_Three()
         {
-
             // Arrange
             var fizbuzz = new MyFizzBuzz();
             var expected = new List<string> { "1", "2", "Fizz" };
@@ -67,26 +67,71 @@ namespace FizzBuzz.Tests
 
         }
 
+        [Test]
+        public void GetFizzBuzz_Should_Return_List_With_One_Two_Fizz_Four_Buzz_If_Input_Is_Five()
+        {
+            // Arrange
+            var fizbuzz = new MyFizzBuzz();
+            var expected = new List<string> { "1", "2", "Fizz", "4", "Buzz" };
+
+            // Act
+            var actual = fizbuzz.GetFizzBuzz(5);
+
+            // Assert
+            CollectionAssert.AreEqual(expected, actual);
+
+        }
+
+        [Test]
+        public void GetFizzBuzz_Should_Return_FizzBuzz_For_15th_Position_If_Input_Is_15_Or_Greater()
+        {
+            // Arrange
+            var fizbuzz = new MyFizzBuzz();
+            var position = 15;
+            var expected = "FizzBuzz";
+
+            // Act
+            var actualFizzBuzzList = fizbuzz.GetFizzBuzz(position);
+            var actual = actualFizzBuzzList.ElementAt(position - 1); // As lists are 0 index based we need to access the 14th item to get the correct position
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
     }
 
     public class MyFizzBuzz
     {
-        public IEnumerable GetFizzBuzz(int value)
+        public List<string> GetFizzBuzz(int value)
         {
             var numbers = new List<string>();
 
             for (var i = 1; i <= value; i++)
             {
-                if (i % 3 == 0)
-                {
-                    numbers.Add("Fizz");
-                }
-                else
-                {
-                    numbers.Add(i.ToString());
-                }
+                numbers.Add(GetFizzBuzzValue(i));
             }
             return numbers;
+        }
+
+        private string GetFizzBuzzValue(int i)
+        {
+            if (i%3 == 0 && i%5 == 0)
+            {
+               return "FizzBuzz";
+            }
+            else if (i%3 == 0)
+            {
+               return "Fizz";
+            }
+            else if (i%5 == 0)
+            {
+               return "Buzz";
+            }
+            else
+            {
+                return i.ToString();
+            }
         }
     }
 }
